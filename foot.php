b353810f4417fcdd1d3821efeb775469094aabe1
Devmoeid <119996337+devmoeid@users.noreply.github.com>
2022-12-10 15:39:33 +0700

    Add files via upload

---
+++ foot.php
@@ -0,0 +1,55 @@
+<?php
+require('./php/info.php');
+?>
+<div class="clear"></div>
+<footer id='footer'>
+<div id="foot_nav">
+<div class="footer-widget">
+<div class="menu-menu-footer-container">
+<ul id="menu-menu-footer" class="menu">
+<li id="menu-item-27538" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-27538"><a href="<?=$webUrl ?>/home">Home</a></li>
+<li id="menu-item-1" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1"><a href="/contact">Contact Us</a></li>
+<li id="menu-item-3" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3"><a href="/18-usc-2257">18 U.S.C. �2257</a></li>
+<li id="menu-item-27539" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-27539"><a href="https://theporndude.com/">ThePornDude</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="content">
+<div class="info">
+<div class="azlist">
+<div class="head">
+<div class="logofooter">
+<img src="<?=$webUrl ?>/assets/img/desunime.png">
+</div>
+</div>
+</div>
+<div class="footer-desc">
+<p>
+Watch Free HD Hentai & Anime Videos <span class="subtext">Enjoy your
+<span class="highlight">unlimited</span>
+hentai & anime collection.  We are the definitive source
+for the best curated 720p / 1080p HD hentai videos, viewable
+by mobile phone and tablet, for free.
+</p>
+</div>
+<div class="socials">
+<a href="<?=$facebook ?>" title="Facebook Fanspage" target="_blank" rel="nofollow"> <i class="fab fa-facebook-f"></i> </a> <a href="<?=$instagram ?>" title="Instagram" target="_blank" rel="nofollow"> <i class="fab fa-instagram"></i> </a> <a href="<?=$discord ?>" title="Discord" target="_blank" rel="nofollow"> <i class="fab fa-discord"></i> </a>
+</div>
+<div class="copyright">
+Copyright © <strong><?=$webName ?></strong>. All Rights Reserved. Powered by <strong>hanime.tv</strong>
+</div>
+</div>
+<div class="clearfix"></div>
+<div class="textlinks"></div>
+<script>
+t=document.querySelector("body>header>nav"),r=document.querySelector("body>header>div>button"),t&&r&&r.addEventListener("click",function(){t.classList.toggle("expand")});
+</script>
+<script>XREADY=false;window.onload=function () {window.App.deferredLoad()}</script>
+<script src="https://cdn.jsdelivr.net/gh/kazudevid/hanime@main/api-player.js" defer="defer"></script>
+</div>
+</div>
+</footer>
+</body>
+</html>
