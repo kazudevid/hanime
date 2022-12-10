4aeb9b9066b140ad88030bb9b052218d6218e083
Devmoeid <119996337+devmoeid@users.noreply.github.com>
2022-12-10 15:38:33 +0700

    Create info.php

---
+++ php/info.php
@@ -0,0 +1,27 @@
+<?php 
+// Website Name
+$webName = "Hanime";
+// Website URL
+$webUrl = "https://hanime.biz.id";
+// Website URL
+$webDomain = "hanime.biz.id";
+//Google Webmaster
+$google = "kuOQulxYSe2Y-a3dK21_TfMKtViOg31rae418k-qFtQ";
+// Google Analytics
+$googletag ="G-P9L8H8XCCJ"; 
+// Yandex Webmaster
+$yandex = "f042b355c631f79c";
+// Bing Webmaster
+$bing = "A169F9FB58AEBE276A240F5A4ECEC82F";
+// Disqus ID
+$disqus = "desunime";
+// Tag Line
+$webTagname = "Watch Free Hentai Video Stream Online in 720p 1080p HD";
+// Sosial Media
+$facebook = "";
+$instagram = "";
+$discord = "";
+// API URL (Don't edit)
+$api = "https://hani.nsdev.ml"; 
+
+?>
